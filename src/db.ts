afff9aa30596fd260ef61b8e1cf1501dead415bc
MohamedEL-gantery <elgantery50@gmail.com>
2024-07-01 19:58:55 +0300

    update db synchronize

--- src/db.ts
+++ src/db.ts
@@ -28,7 +28,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB,
-  synchronize: true,
+  synchronize: false,
   logging: false,
   entities: [
     Blog,
