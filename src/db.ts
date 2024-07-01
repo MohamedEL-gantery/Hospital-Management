4ce8f302142b95ead2611ebcfcac2c18ccb1bada
MohamedEL-gantery <elgantery50@gmail.com>
2024-07-01 16:04:14 +0300

    update db entities migrations

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,30 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import dotenv from 'dotenv';
 
+import { Blog } from './entity/blog';
+import { Booking } from './entity/booking';
+import { Cart } from './entity/cart';
+import { Category } from './entity/category';
+import { ContactUs } from './entity/contactus';
+import { Doctor } from './entity/doctor';
+import { DoctorAwards } from './entity/doctorAwards';
+import { DoctorClinicPhoto } from './entity/doctorClinicPhoto';
+import { DoctorEducation } from './entity/doctorEducation';
+import { DoctorExperience } from './entity/doctorExperience';
+import { Calender } from './entity/doctorCalender';
+import { User } from './entity/user';
+import { UserDependent } from './entity/userDependent';
+import { UserWishlist } from './entity/userWishlist';
+import { Review } from './entity/review';
+import {Hospital1719766323186}from './migrations/1719766323186-hospital'
+
+import dotenv from 'dotenv';
 dotenv.config();
 
-const isProduction = process.env.NODE_ENV === 'production';
+// const isProduction = process.env.NODE_ENV === 'production';
 
-const entitiesPath = isProduction ? 'dist/entity/**/*.js' : 'src/entity/**/*.ts';
-const migrationsPath = isProduction ? 'dist/migrations/**/*.js' : 'src/migrations/**/*.ts';
+// const entitiesPath = isProduction ? 'dist/entity/**/*.js' : 'src/entity/**/*.ts';
+// const migrationsPath = isProduction ? 'dist/migrations/**/*.js' : 'src/migrations/**/*.ts';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -18,7 +35,7 @@ export const AppDataSource = new DataSource({
   database: process.env.DB ,
   synchronize: false,
   logging: false,
-  entities: [entitiesPath],
-  migrations: [migrationsPath],
+  entities: [Blog,Booking,ContactUs,Cart,Category,Doctor,DoctorAwards,DoctorClinicPhoto,DoctorEducation,DoctorExperience,Calender,User,UserDependent,UserWishlist,Review],
+  migrations: [Hospital1719766323186],
   subscribers: [],
 });
